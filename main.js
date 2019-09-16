b64207b07931dc5056810d395d1036f99494172c
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-09-15 21:46:58 -0300

    Add nodemon package

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const app = express()
+const dotenv = require('dotenv').config()
 const port = 3000
+
+app.get('/', function(req, res){
+  res.send('Hello world.');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
