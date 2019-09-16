1f539ffca604eb096b9fa4bead9fc1a49c550630
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-09-15 22:32:17 -0300

    Get quotes from API

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
-var Twit = require('twit')
+const Twit = require('twit')
+const axios = require('axios')
 
 require('dotenv').config()
 
@@ -18,6 +19,24 @@ const T = new Twit({
   access_token_secret:  process.env.ACCESS_TOKEN_SECRET
 })
 
-console.log(T)
+const apiUrl = "http://poetrydb.org/author/Emily Dickinson"
+
+
+// Bring an array of quotes from the API
+const getQuote = async () => {
+  try {
+    const res = await axios.get(apiUrl)
+    const poems = res.data
+
+    //get a random poem from the array
+    const randomPoem = poems[Math.floor(Math.random()*poems.length)].lines
+
+
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+getQuote()
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
