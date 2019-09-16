e508dd7cbfd1f46b5c7f4ddbdfed20fe9cec3937
Florencia Lafuente <lafuente.florencia@gmail.com>
2019-09-15 21:53:21 -0300

    Add dotenv and twit config

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
 const express = require('express')
 const app = express()
-const dotenv = require('dotenv').config()
-const port = 3000
+var Twit = require('twit')
+
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
 
 app.get('/', function(req, res){
   res.send('Hello world.');
 });
+
+// insert your twitter app info here
+const T = new Twit({
+  consumer_key:         process.env.API_KEY, 
+  consumer_secret:      process.env.API_SECRET_KEY,
+  access_token:         process.env.ACCESS_TOKEN,
+  access_token_secret:  process.env.ACCESS_TOKEN_SECRET
+})
+
+console.log(T)
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
